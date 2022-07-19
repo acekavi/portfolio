b67b853093a396e2f361460ed477b49d9794a36d
Avishka Kavinda <dfntlynotace@gmail.com>
2022-07-19 09:58:14 +0530

    Last commit using flask

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def my_home():
+    return render_template('index.html')
+
+@app.route("/<page_name>")
+def html_page(page_name):
+    return render_template(f'{page_name}.html')
